0961516a073d2fa0d3f17fb4eb7f1c87f0e3182d
monthwolf <monthwolf@users.noreply.github.com>
2023-05-23 04:56:44 +0000

    deploy: 1bf742038060a2a68454c56469b5ef4e9b370f25

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: ['@vue/app'],
-  ignore: ['sdk/**'],
-  plugins: [
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-nullish-coalescing-operator'
-  ]
-}
